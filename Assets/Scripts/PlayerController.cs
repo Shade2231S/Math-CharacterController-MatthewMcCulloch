920c8a54e18483644b0c2fbbcbf42e464ee9e0af
Shade2231S <matthewmcculloch149@gmail.com>
2025-11-27 17:04:37 -0400

    PlayerController Progress
    Finished controller functions and methods.
    
    added sprint and crouch to finish controller.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,9 @@ using UnityEngine.UIElements;
 public class PlayerController : MonoBehaviour
 {
     private CharacterController con;
+    private float currentspeed;
     public float walkspeed = 4.5f;
+    public float runspeed = 8f;
     public float jumpforce = 4f;
     public float lookspeed = 4f;
     public float lookxlimit = 85f;
@@ -18,10 +20,12 @@ public class PlayerController : MonoBehaviour
     public Transform groundcheck;
     public Camera camera;
     public bool isground;
+    public bool iscrouch = false;
+    public bool isrunning = false;
     public void Start()
     {
         con = GetComponent<CharacterController>();
-        
+        currentspeed = walkspeed;
     }
     public void Update()
     {
@@ -29,8 +33,8 @@ public class PlayerController : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
         Vector3 move = transform.right * x + transform.forward * y;
-        con.Move(move * walkspeed * Time.deltaTime);
-        move *= walkspeed;
+        con.Move(move * currentspeed * Time.deltaTime);
+        move *= currentspeed;
         velocity.y -= gravity * Time.deltaTime;
         con.Move(velocity * Time.deltaTime);
         if (isground && velocity.y < 0)
@@ -40,6 +44,26 @@ public class PlayerController : MonoBehaviour
         if (Input.GetButtonDown("Jump") && groundplayer)
         {
                 velocity.y = jumpforce;
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            iscrouch = true;
+            con.height = 1f;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            iscrouch = false;
+            con.height = 2f;
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            isrunning = true;
+            currentspeed = runspeed;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            isrunning = false;
+            currentspeed = walkspeed;
         }
         move.y = velocity.y;
         con.Move(move * Time.deltaTime);
