c2030f6f9db78f476406b30cb3d98e7ae61839bc
Shade2231S <matthewmcculloch149@gmail.com>
2025-11-27 14:07:56 -0400

    PlayerController progress
    I have finally made working progress on look and movement, I must of teared apart and put this script back together a least 5 times.
    
    I have researched from multiple source's from unity learn and discussions, to youtube and reddit.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,28 +1,53 @@
 using UnityEngine;
-using UnityEngine.UIElements;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     private CharacterController con;
-    public Rigidbody rb;
     public float walkspeed = 4.5f;
-    public float runspeed = 8;
     public float jumpforce = 4f;
     public float lookspeed = 4f;
     public float lookxlimit = 85f;
-    public float synsitivity = 1f;
     public float gravity = 10f;
     private float lookrotation;
-    Vector2 move, look;
+    Vector3 velocity;
+    public Transform groundcheck;
     public Camera camera;
     public bool isground;
-    public bool canmove = true;
-    public bool isrunning;
-    public bool iswalking;
     public void Start()
     {
         con = GetComponent<CharacterController>();
-        Cursor.lockState = CursorLockMode.Locked;
+        
+    }
+    public void Update()
+    {
+        bool groundplayer = con.isGrounded;
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * y;
+        con.Move(move * walkspeed * Time.deltaTime);
+        move *= walkspeed;
+        velocity.y -= gravity * Time.deltaTime;
+        con.Move(velocity * Time.deltaTime);
+        if (isground && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        if (Input.GetButtonDown("Jump") && groundplayer)
+        {
+                velocity.y = jumpforce;
+        }
+        move.y = velocity.y;
+        con.Move(move * Time.deltaTime);
+        float mouseX = Input.GetAxis("Mouse X") * lookspeed;
+        float mouseY = Input.GetAxis("Mouse Y") * lookspeed;        
+        lookrotation -= mouseY;
+        lookrotation = Mathf.Clamp(lookrotation, -lookxlimit, lookxlimit);
+        camera.transform.localRotation = Quaternion.Euler(lookrotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
     }
 }
--- Assets/Scripts/playercontroller1.cs
+++ Assets/Scripts/playercontroller1.cs
@@ -1,89 +1,93 @@
-using UnityEditor.Experimental.GraphView;
-using UnityEngine;
-using System.Collections;
-using UnityEngine.EventSystems;
-using UnityEngine.InputSystem;
+//using UnityEditor.Experimental.GraphView;
+//using UnityEngine;
+//using System.Collections;
+//using UnityEngine.EventSystems;
+//using UnityEngine.InputSystem;
 
-public class PlayerController1 : MonoBehaviour
-{
-    private CharacterController con;
-    public Rigidbody rb;
-    public float walkspeed = 4.5f;
-    public float runspeed = 8;
-    public float jumpforce = 4f;
-    public float lookspeed = 4f;
-    public float lookxlimit = 85f;
-    public float synsitivity = 1f;
-    public float gravity = 10f;
-    private float lookrotation;
-    Vector2 move, look;
-    public Camera camera;
-    public bool isground;
-    public bool canmove = true;
-    public bool isrunning;
-    public bool iswalking;
-    public void onmove(InputAction.CallbackContext context)
-    {
-        move = context.ReadValue<Vector2>();
-    }
-    public void onlook(InputAction.CallbackContext context)
-    {
-        look = context.ReadValue<Vector2>();
-    }
-    public void Move()
-    {
+//public class PlayerController : MonoBehaviour
+//{
+//    private CharacterController con;
+//    public Rigidbody rb;
+//    public float walkspeed = 4.5f;
+//    public float runspeed = 8;
+//    public float jumpforce = 4f;
+//    public float lookspeed = 4f;
+//    public float lookxlimit = 85f;
+//    public float synsitivity = 1f;
+//    public float gravity = 10f;
+//    private float lookrotation;
+//    Vector2 move, look;
+//    public Camera camera;
+//    public bool isground;
+//    public bool canmove = true;
+//    public bool isrunning;
+//    public bool iswalking;
+//    public void onmove(InputAction.CallbackContext context)
+//    {
+//        move = context.ReadValue<Vector2>();
+//    }
+//    public void onlook(InputAction.CallbackContext context)
+//    {
+//        look = context.ReadValue<Vector2>();
+//    }
+//    public void Move()
+//    {
 
-        Vector3 currentvel = rb.linearVelocity;
-        Vector3 targetvel = new Vector3(move.x, 0, move.y);
-        targetvel *= walkspeed;
-        targetvel = transform.TransformDirection(targetvel);
-        Vector3 velchange = (currentvel - targetvel);
-        velchange = new Vector3(velchange.x, velchange.y, velchange.z);
-        Vector3.ClampMagnitude(velchange, 4.5f);
-        rb.AddForce(velchange, ForceMode.VelocityChange);
-    }
-    public void Look()
-    {
-        transform.Rotate(Vector3.up * look.x * synsitivity);
-        lookrotation += (-look.y * synsitivity);
-        lookrotation = Mathf.Clamp(lookrotation, -lookxlimit, lookxlimit);
-        camera.transform.eulerAngles = new Vector3(lookrotation, camera.transform.eulerAngles.y, camera.transform.eulerAngles.z);
-    }
-    void Start()
-    {
-        con = GetComponent<CharacterController>();
-        camera = GetComponent<Camera>();
-        Cursor.lockState = CursorLockMode.Locked;        
-    }
+//        Vector3 currentvel = rb.linearVelocity;
+//        Vector3 targetvel = new Vector3(move.x, 0, move.y);
+//        targetvel *= walkspeed;
+//        targetvel = transform.TransformDirection(targetvel);
+//        Vector3 velchange = (currentvel - targetvel);
+//        velchange = new Vector3(velchange.x, velchange.y, velchange.z);
+//        Vector3.ClampMagnitude(velchange, walkspeed);
+//        rb.AddForce(velchange, ForceMode.VelocityChange);
+//    }
+//    public void Look()
+//    {
+//        //transform.Rotate(Vector3.up * look.x * synsitivity);
+//        //lookrotation += (-look.y * synsitivity);
+//        //lookrotation = Mathf.Clamp(lookrotation, -lookxlimit, lookxlimit);
+//        //camera.transform.eulerAngles = new Vector3(lookrotation, camera.transform.eulerAngles.y, camera.transform.eulerAngles.z);
+//    }
+//    void Start()
+//    {
+//        con = GetComponent<CharacterController>();
+//        camera = GetComponent<Camera>();
+//        Cursor.lockState = CursorLockMode.Locked;        
+//    }
 
-    public void onjump(InputAction.CallbackContext context)
-    {
-        jump();
-    }
-    public void jump() 
-    {
-        //Vector3 jumpforces = Vector3.zero;
-        //if (isground == true)
-        //{
-        //    jumpforces = Vector3.up * jumpforce;
-        //}
-        //rb.AddForce(jumpforces, ForceMode.VelocityChange);
-    }
-    void FixedUpdate()
-    {
+//    public void onjump(InputAction.CallbackContext context)
+//    {
+//        jump();
+//    }
+//    public void jump() 
+//    {
+//        //Vector3 jumpforces = Vector3.zero;
+//        //if (isground == true)
+//        //{
+//        //    jumpforces = Vector3.up * jumpforce;
+//        //}
+//        //rb.AddForce(jumpforces, ForceMode.VelocityChange);
+//    }
+//    void FixedUpdate()
+//    {
         
-    }
-    void Update()
-    {
-        Move();
-        Look();
-    }
-    public void crouch()
-    {
+//    }
+//    void FixedUpdate()
+//    {
+//        Move();
+        
+//    }
+//    void LateUpdate()
+//    {
+//        Look();
+//    }
+//    public void crouch()
+//    {
 
-    }
-    public void oncrouch(InputAction.CallbackContext context)
-    {
+//    }
+//    public void oncrouch(InputAction.CallbackContext context)
+//    {
 
-    }
-}
+//    }
+//}
